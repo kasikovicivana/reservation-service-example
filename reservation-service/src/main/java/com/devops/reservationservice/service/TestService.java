3bdaf2590909fda2ebaf6f4c797f7a0cd60e7116
Ivana Kasikovic <ikasikovic@yahoo.com>
2024-05-01 20:04:22 +0200

    Test unit tests in pipeline

---
+++ reservation-service/src/main/java/com/devops/reservationservice/service/TestService.java
@@ -0,0 +1,11 @@
+package com.devops.reservationservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+    public String test(){
+        return "Welcome from reservation-service";
+    }
+}
+
--- reservation-service/src/test/java/com/devops/reservationservice/ReservationServiceApplicationTests.java
+++ reservation-service/src/test/java/com/devops/reservationservice/ReservationServiceApplicationTests.java
@@ -1,13 +1,23 @@
 package com.devops.reservationservice;
 
+import com.devops.reservationservice.service.TestService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ReservationServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testService(){
+		System.out.println("test krece");
+		TestService service = new TestService();
+		String res = service.test();
+		assertEquals("Welcome from reservation-service", res);
 	}
 
 }
