3bcbcf31e0854801d1eef04c3e9c2f1d06349c2f
Ivana Kasikovic <ikasikovic@yahoo.com>
2024-05-04 17:02:49 +0200

    Fix tests

--- reservation-service/src/test/java/com/devops/reservationservice/unit/ReservationServiceApplicationTests.java
+++ reservation-service/src/test/java/com/devops/reservationservice/unit/ReservationServiceApplicationTests.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(MockitoExtension.class)
 class ReservationServiceApplicationTests {
 
