7dc432aa772c79607588940824ed03e18bd091d5
Ivana Kasikovic <ikasikovic@yahoo.com>
2024-05-04 16:33:15 +0200

    Try

--- reservation-service/src/test/java/com/devops/reservationservice/unit/ReservationServiceApplicationTests.java
+++ reservation-service/src/test/java/com/devops/reservationservice/unit/ReservationServiceApplicationTests.java
@@ -4,6 +4,7 @@ import com.devops.reservationservice.repository.TestRepository;
 import com.devops.reservationservice.service.TestService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +16,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(MockitoExtension.class)
 class ReservationServiceApplicationTests {
 
-    @Autowired
-    TestService testService;
 
     @Mock
     TestRepository testRepository;
+
+    @InjectMocks
+    TestService testService;
 
     @Test
     void contextLoads() {
