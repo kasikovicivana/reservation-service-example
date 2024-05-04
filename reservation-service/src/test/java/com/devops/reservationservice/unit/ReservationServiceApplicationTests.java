3afbe0fa8debfc5f7fcf3d527b16988f16954aa5
Ivana Kasikovic <ikasikovic@yahoo.com>
2024-05-04 16:20:53 +0200

    Try to mock the repo

--- reservation-service/src/test/java/com/devops/reservationservice/unit/ReservationServiceApplicationTests.java
+++ reservation-service/src/test/java/com/devops/reservationservice/unit/ReservationServiceApplicationTests.java
@@ -1,17 +1,25 @@
 package com.devops.reservationservice.unit;
 
+import com.devops.reservationservice.repository.TestRepository;
 import com.devops.reservationservice.service.TestService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(MockitoExtension.class)
 class ReservationServiceApplicationTests {
 
     @Autowired
     TestService testService;
+
+    @Mock
+    TestRepository testRepository;
 
     @Test
     void contextLoads() {
